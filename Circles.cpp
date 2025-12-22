73790a42f3e8312199bb3289f02986345de849b1
sara omari <148950450+SaraOmari@users.noreply.github.com>
2025-12-22 15:13:02 +0300

    Update Circles.cpp

--- Circles.cpp
+++ Circles.cpp
@@ -9,6 +9,7 @@ int main()
        long long a,b,d; 
        cin>>a>>b>>d; 
        double ans=d*d/2.0; 
-       cout<<fixed<<setprecision(6)<<ans<<endl; }
+       cout<<fixed<<setprecision(6)<<ans<<endl; 
+    }
     return 0;
 }
