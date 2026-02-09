a0b12aa94aa5afe2535b530fd5ead203fdfdaaaa
sara omari <148950450+SaraOmari@users.noreply.github.com>
2026-02-09 18:28:02 +0300

    Create contain duplicate.cpp

---
+++ contain duplicate.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <list>
+#include <algorithm>
+using namespace std; 
+int main()
+
+{  
+    string s;
+    cin>>s; 
+    unordered_map<char,int>mp;
+    for(char c:s)
+    mp[c]++;
+   int flag=0;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(mp[s[i]]>1){
+            flag=1; 
+            break;
+        }
+     
+        
+    }
+    if(flag)
+    cout<<"true"<<endl; 
+    else
+    cout<<"false\n"; 
+    
+    return 0;}
