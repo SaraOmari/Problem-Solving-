c7465e10f135d501689caa97c861c7e44a1e09c0
sara omari <148950450+SaraOmari@users.noreply.github.com>
2026-04-10 17:21:13 +0300

    Create div3.cpp

---
+++ div3.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+
+int n; cin>>n; 
+int arr[n]; 
+unordered_map<int,int>m;
+for(int i=0; i<n; i++){
+cin>>arr[i]; 
+ m[arr[i]]++;
+}
+int mx=0;
+int ans;
+
+for(auto x:m){
+  if(x.second>mx){
+      mx=x.second;
+    
+  }
+}
+cout<<mx;
+return 0;
+}
